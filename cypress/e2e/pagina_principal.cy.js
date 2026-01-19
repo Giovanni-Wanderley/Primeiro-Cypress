512ed4b2e8c82b119f78aab6549b5efa8fcad3d3
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-01-18 21:48:23 -0300

    verifica se o titulo 'Adopet' esta presente no html

--- cypress/e2e/pagina_principal.cy.js
+++ cypress/e2e/pagina_principal.cy.js
@@ -1,6 +1,14 @@
-describe("Deve retornar a página principal", () => {
-  it("Visita a página principal do AdoPet e testa o botão de home", () => {
+describe("Página Principal", () => {
+  beforeEach(() => {
     cy.visit("https://adopet-frontend-cypress.vercel.app/");
+  });
+
+  it('Deve verificar o título da página e clicar no botão "Ver pets disponíveis para adoção"', () => {
+    cy.title().should("eq", "AdoPet");
+    cy.get(".button").click();
+  });
+
+  it("Deve testar o botão de home no cabeçalho e garantir o retorno à página principal", () => {
     cy.get(".header__home").click();
     cy.url().should("eq", "https://adopet-frontend-cypress.vercel.app/");
   });
