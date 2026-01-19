b93eb9ad310b64c4b5f4ff353c23c7e6bcfb3f20
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-01-18 21:53:19 -0300

    verifica se elementos de texto estao visiveis na pagina

--- cypress/e2e/pagina_principal.cy.js
+++ cypress/e2e/pagina_principal.cy.js
@@ -12,4 +12,14 @@ describe("Página Principal", () => {
     cy.get(".header__home").click();
     cy.url().should("eq", "https://adopet-frontend-cypress.vercel.app/");
   });
+
+  it('Deve verificar se o texto "Quem ama adota!" está presente no html', () => {
+    cy.contains("Quem ama adota!").should("be.visible");
+  });
+
+  it("Deve verificar se o texto explicativo sobre adoção está presente no html", () => {
+    cy.contains(
+      "Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!",
+    ).should("be.visible");
+  });
 });
