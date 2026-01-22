8d2f89cdd25fcfbe833ce32bf025f91e0b7f46d9
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-01-21 22:22:32 -0300

    chore: configura retries e timeouts globais

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    defaultCommandTimeout: 6000,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
