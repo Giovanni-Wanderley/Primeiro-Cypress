9031bf4d4043e487d1d2d794e49ea2f8fffb6307
Giovanni Luiz da Silva Wanderley <115003765+Giovanni-Wanderley@users.noreply.github.com>
2026-01-22 23:05:39 -0300

    Revert "Chore/config cypress"

--- cypress.config.js
+++ cypress.config.js
@@ -2,23 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    retries: {
-      runMode: 2,
-      openMode: 0,
-    },
-    defaultCommandTimeout: 6000,
-
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    video: true,
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/results",
-      overwrite: false,
-      html: true,
-      json: false,
-      timestamp: "mmddyyyy_HHMMss",
     },
   },
 });
