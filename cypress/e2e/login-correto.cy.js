0246d17f55f86f475cb5a18db6e8dc84131b9972
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-01-22 23:48:49 -0300

    refatora login-correto para usar comando customizado e garante estabilidade da url

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -5,11 +5,8 @@ describe("Página de Login", () => {
   });
 
   it("Deve preencher os campos do formulário corretamente para fazer login", () => {
-    cy.login(dados.email, senha);
     cy.fixture("usuario").then((dados) => {
-      cy.get('[data-test="input-loginEmail"]').type(dados.email);
-      cy.get('[data-test="input-loginPassword"]').type(senha);
-      cy.get('[data-test="submit-button"]').click();
+      cy.login(dados.email, "Senha123");
       cy.contains("p", "Olá!").should("be.visible");
     });
   });
