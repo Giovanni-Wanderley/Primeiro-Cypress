b72c3d6ab8cc5158452e39a6af992866798af69a
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-01-24 21:23:31 -0300

    implementa comando personalizado 'cadastrar' e refatora teste de cadastro

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -2,16 +2,15 @@ describe("Página de Cadastro", () => {
   beforeEach(() => {
     cy.visit("https://adopet-frontend-cypress.vercel.app/");
     cy.get('[data-test="register-button"]').click();
+    cy.contains("p", "Ainda não tem cadastro?").should("be.visible");
   });
 
   it("Preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
     const emailDinamico = `giovanni${Date.now()}@email.com`;
-    cy.get('[data-test="input-name"]').type("Giovanni Wanderley");
-    cy.get('[data-test="input-email"]').type(emailDinamico);
-    cy.get('[data-test="input-password"]').type("Senha123");
-    cy.get('[data-test="input-confirm-password"]').type("Senha123");
+    const senhaPadrao = "Senha123";
+
     cy.writeFile("cypress/fixtures/usuario.json", { email: emailDinamico });
-    cy.get('[data-test="submit-button"]').click();
+    cy.cadastrar("Giovanni Wanderley", emailDinamico, senhaPadrao);
     cy.url().should("include", "/login");
   });
 });
