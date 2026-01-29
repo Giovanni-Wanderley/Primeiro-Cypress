5589093e8e1851d9f863ef5f46b5fab53e52b4b6
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-01-28 23:14:18 -0300

    build: sincroniza workflow e limpa arquivos duplicados

--- estudo_cypress/cypress/e2e/pagina_home.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Página Home', () => {
-  it('Deve acessar a página home e página de "Falar com o responsável"', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/home');
-    cy.get('.header__message').click() 
-  });
-});
--- estudo_cypress/cypress/e2e/pets_disponiveis.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Pets Disponíveis', () => {
-  it('Deve exibir a lista de pets disponíveis após clicar no botão ‘Ver pets disponíveis para adoção”', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.contains('a', 'Ver pets disponíveis para adoção').click();
-  });
-
-});
