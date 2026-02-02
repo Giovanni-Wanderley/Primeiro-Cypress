f6f5930e78dffb6a8e3b2ef17cac4d4fe45275a0
Giovanni Wanderley <luiz17giovanni@gmail.com>
2026-02-01 21:09:38 -0300

    fix

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -5,13 +5,19 @@ describe("Página de Cadastro", () => {
     cy.contains("p", "Ainda não tem cadastro?").should("be.visible");
   });
 
-  it("Preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
-    const emailDinamico = `giovanni${Date.now()}@email.com`;
-    const senhaPadrao = "Senha123";
+  it("Deve cadastrar um usuário com nome aleatório e e-mail dinâmico", () => {
+    cy.fixture("massa-dados").then((massa) => {
+      const nomeAleatorio =
+        massa.cadastro.nomes[Math.floor(Math.random() * massa.cadastro.nomes.length)];
+      const emailDinamico = `email_aleatorio${Date.now()}@email.com`;
 
-    cy.writeFile("cypress/fixtures/usuario.json", { email: emailDinamico });
-    cy.cadastrar("Giovanni Wanderley", emailDinamico, senhaPadrao);
-    cy.contains("p", "Já tem conta? Faça seu login:").should("be.visible");
-    cy.url().should("include", "/login");
+      cy.writeFile("cypress/fixtures/usuario.json", {
+        email: emailDinamico,
+        nome: nomeAleatorio,
+      });
+      cy.cadastrar(nomeAleatorio, emailDinamico, massa.cadastro.senha);
+      cy.url().should("include", "/login");
+      cy.contains("p", "Já tem conta? Faça seu login:").should("be.visible");
+    });
   });
 });
